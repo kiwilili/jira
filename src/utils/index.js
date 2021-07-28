e5686d040758a4e6c76f7ea73fba9bec423a32ca
DESKTOP-94CP6N1\kiwi <kiwilili@163.com>
2021-07-28 10:53:59 +0800

    feat: 添加 useMount 和 useDebounce 两个 hook

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+
 export const isFalsy = (value) => (value === 0 ? false : !value);
 export const cleanObject = (object) => {
   const result = { ...object };
@@ -8,4 +10,23 @@ export const cleanObject = (object) => {
     }
   });
   return result;
+};
+
+// hook 只能在组件中、或者hook中调用
+export const useMount = (callback) => {
+  useEffect(() => {
+    callback();
+  }, []);
+};
+
+export const useDebounce = (value, delay) => {
+  const [debounceValue, setDebounceValue] = useState(value);
+
+  useEffect(() => {
+    // 每次在 value 变化以后，设置一个定时器
+    const timeout = setTimeout(() => setDebounceValue(value), delay);
+    // 每次在上一个 useEffect 处理完以后在运行
+    return () => clearTimeout(timeout);
+  }, [value, delay]);
+  return debounceValue;
 };
