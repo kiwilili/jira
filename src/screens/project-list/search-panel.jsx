49d455675be32c915391fb246bbc4c654ec056a4
DESKTOP-94CP6N1\kiwi <kiwilili@163.com>
2021-07-28 10:21:11 +0800

    feat: 添加工程列表

--- src/screens/project-list/search-panel.jsx
+++ src/screens/project-list/search-panel.jsx
@@ -1,35 +1,19 @@
-import { useEffect, useState } from "react"
+export const SearchPanel = ({users, param, setParam}) => {
 
-export const SearchPanel = () => {
-  const [param, setParam] = useState({
-    name: '',
-    personId: ''
-  })
-  const [users, setUsers] = useState([])
-  const [list, setList] = useState([])
-
-  useEffect(() => {
-    fetch('').then(async response => {
-      if (response.ok) {
-        setList(await response.json())
-      }
-    })
-  }, [param])
-
-  return <form action="">
+  return <form>
     <div>
-      {/* setParam{Object.assign({}, param, { name: evt.target.value })} */}
+      {/*setParam(Object.assign({}, param, {name:evt.target.value}))*/}
       <input type="text" value={param.name} onChange={evt => setParam({
         ...param,
         name: evt.target.value
-      })} />
-      <select value={param.personId} onChange={ evt => setParam({
+      })}/>
+      <select value={param.personId} onChange={evt => setParam({
         ...param,
         personId: evt.target.value
       })}>
         <option value={''}>负责人</option>
         {
-          users.map(user => <option value={user.id}>{user.name}</option>)
+          users.map(user => <option key={user.id} value={user.id}>{user.name}</option>)
         }
       </select>
     </div>
