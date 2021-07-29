51b987e26cd42e2acf4a003d837d9f7117679ac3
DESKTOP-94CP6N1\kiwi <kiwilili@163.com>
2021-07-29 11:41:54 +0800

    style: 安装 emotion, 优化登录注册界面

--- src/unauthenticated-app/register.tsx
+++ src/unauthenticated-app/register.tsx
@@ -1,5 +1,6 @@
 import { useAuth } from "context/auth-context"
-import {Button, Form, Input} from 'antd'
+import { Form, Input} from 'antd'
+import { LongButton } from "unauthenticated-app"
 
 export const RegisterScreen = () => {
   const {register} = useAuth()
@@ -16,7 +17,7 @@ export const RegisterScreen = () => {
       <Input placeholder='密码' type="password" id={'password'} />
     </Form.Item>
     <Form.Item>
-      <Button htmlType={'submit'} type={'primary'}>注册</Button>
+      <LongButton htmlType={'submit'} type={'primary'}>注册</LongButton>
     </Form.Item>
   </Form>
 }
