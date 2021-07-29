477f94a78f0b5543474881269fb80e69f38dfb2b
DESKTOP-94CP6N1\kiwi <kiwilili@163.com>
2021-07-29 10:56:40 +0800

    Style: 增加 antd

--- src/screens/project-list/list.tsx
+++ src/screens/project-list/list.tsx
@@ -1,3 +1,4 @@
+import { Table } from "antd";
 import { User } from "./search-panel";
 interface Project {
   id: string,
@@ -11,21 +12,21 @@ interface ListProps {
   users: User[]
 }
 export const List = ({list, users}: ListProps) => {
-  return <table>
-    <thead>
-    <tr>
-      <th>名称</th>
-      <th>负责人</th>
-    </tr>
-    </thead>
-    <tbody>
+  return <Table pagination={false} columns={[
     {
-      list.map(project => <tr key={project.id}>
-        <td>{project.name}</td>
-        {/*undefined.name*/}
-        <td>{users.find(user => user.id === project.personId)?.name || '未知'}</td>
-      </tr>)
+      title: '名称',
+      dataIndex: 'name',
+      sorter: (a, b) => a.name.localeCompare(b.name)
+    },
+    {
+      title: '名称',
+      render(value, project) {
+        return <span>
+          {users.find(user => user.id === project.personId)?.name || '未知'}
+        </span>
+      }
     }
-    </tbody>
-  </table>
+  ]} dataSource={list} rowKey={'id'}>
+
+  </Table>
 }
