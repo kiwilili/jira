a2d11e3fa50eb76fa4f285fd69e66170971848a6
DESKTOP-94CP6N1\kiwi <kiwilili@163.com>
2021-08-03 17:37:24 +0800

    feat: 用useAsync处理loading和error状态

--- src/screens/project-list/index.tsx
+++ src/screens/project-list/index.tsx
@@ -1,32 +1,26 @@
 import {SearchPanel} from "./search-panel";
 import {List} from "./list";
 import {useState} from "react";
-import {useDebounce, useMount} from "../../utils";
-import { useHttp } from "utils/http";
+import {useDebounce} from "../../utils";
 import styled from "@emotion/styled";
 import { Typography } from "antd";
 import { useProjects } from "utils/project";
+import { useUsers } from "utils/user";
 
 export const ProjectListScreen = () => {
-  const [users, setUsers] = useState([])
-
   const [param, setParam] = useState({
     name: '',
     personId: ''
   })
   const debouncedParam = useDebounce(param, 200)
-  const client = useHttp()
   const {isLoading, error, data: list} = useProjects(debouncedParam)
-
-  useMount(() => {
-    client('users').then(setUsers)
-  })
+  const {data: users} = useUsers()
 
   return <Container>
     <h1>项目列表</h1>
-    <SearchPanel users={users} param={param} setParam={setParam}/>
+    <SearchPanel users={users || []} param={param} setParam={setParam}/>
     {error ? <Typography.Text type={'danger'}>{error.message}</Typography.Text>: null }
-    <List loading={isLoading} users={users} dataSource={list || []}/>
+    <List loading={isLoading} users={users || []} dataSource={list || []}/>
   </Container>
 }
 
